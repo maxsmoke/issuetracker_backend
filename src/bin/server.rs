cd3cf7af00a3e27c726ada408f79e0a0a07d2c53
Gian Clemons <gian.d.clemons@gmail.com>
2019-12-15 12:53:53 -0500

    refactored query_projects fn

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,6 +1,5 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
-// #[macro_use]
 extern crate serde;
 
 #[macro_use]
@@ -35,18 +34,22 @@ fn get_projects() -> Json<JsonProjectResponse>{
 // #[get("/project/<id>")]
 #[get("/project?<id>")]
 fn query_projects(id: Option<&RawStr>) -> Json<JsonProjectResponse> {
-    let raw_str = id.unwrap();
-    let decode = raw_str.url_decode().unwrap();
-    let int_id = decode.parse::<i32>().unwrap();
-    
-    let mut response = JsonProjectResponse{ data: vec![] };
 
     let conn = establish_connection();
 
+    let raw_str = match id {
+        Some(e) =>  e,
+        None => RawStr::from_str("0"),
+    };
+
+    let int_id = match raw_str.url_decode().unwrap().parse::<i32>(){
+        Ok(e) => e,
+        Err(_e) => 0,
+    };
+    
     let result = models::Project::query_projects(&conn, int_id);
 
-    response.data.push(result);
-    Json(response)
+    Json( JsonProjectResponse{ data: vec![result] })
 }
 
 #[get("/issues")]
